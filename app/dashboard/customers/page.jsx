da125773cd0fe25253ac872e4cb8b1f3ffa78138
Rashad <akyrashad@gmail.com>
2023-12-22 09:41:06 +0530

    Next js dashboard app completed

--- app/dashboard/customers/page.jsx
+++ app/dashboard/customers/page.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const page = () => {
-  return (
-    <p>Customer Page</p>
-  )
-}
+  return <p>Customer Page</p>;
+};
 
-export default page
+export default page;
--- app/dashboard/invoices/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <p>Invoice page</p>
-  )
-}
-
-export default page
